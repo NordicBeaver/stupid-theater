8df9db5bc6e4df838831c050374d0e297acde279
NordicBeaver <nordicbeaver@gmail.com>
2023-03-15 09:23:55 +0200

    Set up fastify.

--- stupid-theater-server/src/main.ts
+++ stupid-theater-server/src/main.ts
@@ -1,1 +1,17 @@
-console.log('Hello!');
+import fastify from 'fastify';
+
+const server = fastify({ logger: true });
+
+server.get('/', async (requiest, response) => {
+  return { hello: 'H!' };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3001 });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+start();
