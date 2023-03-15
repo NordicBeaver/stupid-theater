9882e8e6c9c60f76087001ef61f01465f1eecf7a
NordicBeaver <nordicbeaver@gmail.com>
2023-03-15 18:27:29 +0200

    Set fastify host.

--- stupid-theater-server/src/main.ts
+++ stupid-theater-server/src/main.ts
@@ -12,7 +12,7 @@ server.register(charactersRouter, { prefix: '/characters' });
 
 const start = async () => {
   try {
-    await server.listen({ port: 3001 });
+    await server.listen({ port: 3001, host: '0.0.0.0' });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
